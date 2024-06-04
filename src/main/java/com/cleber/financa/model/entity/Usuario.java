da06ef2ed02aeb7192c3326263823e176129990d
garzaro <clebergarzaro74@gmail.com>
2024-06-04 15:35:42 -0300

    adicionado os atributos

--- src/main/java/com/cleber/financa/model/entity/Usuario.java
+++ src/main/java/com/cleber/financa/model/entity/Usuario.java
@@ -1,6 +1,15 @@
 package com.cleber.financa.model.entity;
 
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +22,31 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name= "usuario", schema = "financeiro")
-public class Usuario{
-		
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "nome_completo")
+	private String nomeCompleto;
+	
+	@Column(name = "nome_usuario")
+	private String nomeUsuario;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "senha")
+	private String senha;
+	
+	@Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
+	@Column(name = "data_cadastro")
+	private LocalDate dataCadastro;
+	
+	/*getters and setters*/
+	/*hashcode and equals*/
+	/*toString*/
 
 }
