e30d3e79d843d30e310d38351839e8cac6ea8eaf
garzaro <clebergarzaro74@gmail.com>
2024-06-04 16:30:38 -0300

    adicionado repository do usuario

---
+++ src/main/java/com/cleber/financa/model/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.cleber.financa.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cleber.financa.model.entity.Usuario;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+    
+    /*existe um usuario com um email*/
+    boolean existsByEmail(String email);
+    
+    /*busca um usuario por email*/
+    Optional<Usuario> findByEmail(String email);
+}
