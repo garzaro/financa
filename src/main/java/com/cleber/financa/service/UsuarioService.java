f0c351c4e501e67762e90e4987c9776bbe09c9c5
garzaro <clebergarzaro74@gmail.com>
2024-06-04 16:31:21 -0300

    adicionado servico do usuario

---
+++ src/main/java/com/cleber/financa/service/UsuarioService.java
@@ -0,0 +1,16 @@
+package com.cleber.financa.service;
+
+import com.cleber.financa.model.entity.Usuario;
+
+public interface UsuarioService {
+	
+	/*verificar se o usuario existe na base, validação*/
+	Usuario validarLogin(String email, String senha); 
+	
+	/*salvar o usuario na base*/
+	Usuario persistirUsuarioNabaseDeDados(Usuario usuario);
+	
+	/*verifica o email na base de dados, unique */
+	void validarEmailNaBaseDedados(String email);
+
+}
