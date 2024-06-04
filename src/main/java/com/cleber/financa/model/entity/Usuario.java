6797677f36655a63a1d3b323a663f9de85948a96
garzaro <clebergarzaro74@gmail.com>
2024-06-04 14:45:18 -0300

    commit inicial

---
+++ src/main/java/com/cleber/financa/model/entity/Usuario.java
@@ -0,0 +1,19 @@
+package com.cleber.financa.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name= "usuario", schema = "financeiro")
+public class Usuario{
+		
+
+}
