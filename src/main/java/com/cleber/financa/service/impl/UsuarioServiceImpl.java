66f6524f0e5751179000631c1f9a9526d3a20abb
garzaro <clebergarzaro74@gmail.com>
2024-06-04 20:39:19 -0300

    inicio implementação

--- src/main/java/com/cleber/financa/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/cleber/financa/service/impl/UsuarioServiceImpl.java
@@ -1,15 +1,39 @@
 package com.cleber.financa.service.impl;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cleber.financa.exception.ErroDeValidacacao;
+import com.cleber.financa.exception.RegraDeNegocioException;
 import com.cleber.financa.model.entity.Usuario;
+import com.cleber.financa.model.repository.UsuarioRepository;
 import com.cleber.financa.service.UsuarioService;
 
 public class UsuarioServiceImpl implements UsuarioService{
+	
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+		super();
+		this.usuarioRepository = usuarioRepository;
+	}
 
 	@Override
 	public Usuario validarLogin(String email, String senha) {
-		// TODO Auto-generated method stub
-		return null;
+		/*login*/
+		Optional<Usuario> validandoLogin = usuarioRepository.findByEmail(email);
+		/*verificar a existencia de usuario na base de dados*/
+		if(!validandoLogin.isPresent()) {
+			throw new ErroDeValidacacao("O email não foi encontrado");
+		}
+		if(!validandoLogin.get().getSenha().equals(senha)) {
+			
+		}
+		return validandoLogin.get();
 	}
+	
 
 	@Override
 	public Usuario persistirUsuarioNabaseDeDados(Usuario usuario) {
@@ -19,7 +43,11 @@ public class UsuarioServiceImpl implements UsuarioService{
 
 	@Override
 	public void validarEmailNaBaseDedados(String email) {
-		// TODO Auto-generated method stub
+		/*ver se o email existe*/
+		boolean verificarSeOEmailExisteNaBAseDeDados = usuarioRepository.existsByEmail(email);
+		if(verificarSeOEmailExisteNaBAseDeDados) {
+			throw new RegraDeNegocioException("Já existe um usuario com esse email");
+		}
 		
 	}
 
