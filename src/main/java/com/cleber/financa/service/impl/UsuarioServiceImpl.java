c625691735029cc91b01b0bb01eb3294eeb725e8
garzaro <clebergarzaro74@gmail.com>
2024-06-04 16:32:26 -0300

    adicionado implementação de servico do usuario

---
+++ src/main/java/com/cleber/financa/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cleber.financa.service.impl;
+
+import com.cleber.financa.model.entity.Usuario;
+import com.cleber.financa.service.UsuarioService;
+
+public class UsuarioServiceImpl implements UsuarioService{
+
+	@Override
+	public Usuario validarLogin(String email, String senha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario persistirUsuarioNabaseDeDados(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void validarEmailNaBaseDedados(String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
