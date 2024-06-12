6cabbc2f6064f682effaffc74eace927eaf2dd0e
garzaro <clebergarzaro74@gmail.com>
2024-06-11 21:26:44 -0300

    adicionado @Repository

--- src/main/java/com/cleber/financa/model/repository/UsuarioRepository.java
+++ src/main/java/com/cleber/financa/model/repository/UsuarioRepository.java
@@ -2,10 +2,13 @@ package com.cleber.financa.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+
 import com.cleber.financa.model.entity.Usuario;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
     /*existe um usuario com um email*/
@@ -14,4 +17,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     /*busca um usuario por email*/
     Optional<Usuario> findByEmail(String email);
     
+       
 }
